2581dc283f249a49cdf9ee8a53efeb91c82b0629
Katherine Corkery <kcorker2@asu.edu>
2024-08-21 10:55:30 +0530

    Update page.jsx add current path

--- app/(root)/(home)/(routes)/category/page.jsx
+++ app/(root)/(home)/(routes)/category/page.jsx
@@ -9,6 +9,7 @@ const Mycategory = () => {
   return (
     <div>
       <h1 className="select_header">Select Category</h1>
+<small className="text-gray-400">Path: {course}</small> {/* Display the "name" query parameter */}
       <div className="item-center">
         <div className="grid grid-cols-2 mt-[18px] gap-[18px]">
           {category.map((category, index) => {
