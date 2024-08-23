9d61edbaf763062274aaf4281c7bc4fbb48c2f09
Katherine Corkery <kcorker2@asu.edu>
2024-08-23 17:35:16 +0530

    Update Sidebar.jsx remove serch

--- components/navigation/Sidebar.jsx
+++ components/navigation/Sidebar.jsx
@@ -1,51 +1,192 @@
+/* eslint-disable @next/next/no-page-custom-font */
 "use client";
 
-import { useState } from "react";
-
-import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect, useMemo, useRef, useState } from "react";
 
+import Search from "../Search";
 import { navlinks } from "@/constants";
-import { logo, sun } from "@/public/assets";
-import Icon from "../ui/Icon";
+import { usePost } from "@/libs/hooks/usePost";
+import { filterPosts } from "@/libs/hooks/usefilter";
+import { close, logo, menu } from "@/public/assets";
+import { usePostStore } from "@/libs/state/useStore";
 import ShareDialogBox from "../models/ShareDialogBox";
+import PostViewDialogBox from "../models/PostViewDialogBox";
 
-const Sidebar = () => {
+const Navbar = () => {
+  const { data: fetchedData, error } = usePost();
+  const setData = usePostStore((state) => state.setPosts);
+
   const router = useRouter();
-  const [isActive, setIsActive] = useState("Home");
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <aside className="flex justify-between items-center flex-col sticky top-5 h-[89vh]">
-      <Link href="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
-      </Link>
+  const sidebarRef = useRef(null);
+  const menuButtonRef = useRef(null);
 
-      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
-        <div className="flex flex-col justify-center items-center gap-3 ">
-          {navlinks.map((data) => {
-            return (
-              <div
-                key={data.name}
-                className="tooltip tooltip-right"
-                data-tip={data.name}
-              >
-                <Icon
-                  {...data}
-                  isActive={isActive}
-                  handleClick={() => {
-                    setIsActive(data.name);
-                    data.btn ? setIsOpen(true) : router.push(data.link);
-                  }}
-                />
-              </div>
-            );
-          })}
+  const [post, setPost] = useState("");
+  const [isPostOpen, setIsPostOpen] = useState(false);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isActive, setIsActive] = useState("Home");
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+
+  // For search function
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  // For controlling fetched data
+  const [posts, setPosts] = useState([]);
+  const data = useMemo(() => posts, [posts]);
+
+  useEffect(() => {
+    setData(posts);
+  }, [posts, setData]);
+
+  useEffect(() => {
+    if (fetchedData) {
+      setPosts(fetchedData);
+    }
+    if (error) {
+      console.error("Error fetching Search data:", error);
+    }
+  }, [fetchedData, error]);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPosts(e.target.value, data);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  // For navigation
+  const handleToggleDrawer = () => {
+    setToggleDrawer((prev) => !prev);
+  };
+
+  const handleCloseSidebar = () => {
+    setToggleDrawer(false);
+  };
+
+  const handleCloseSearch = () => {
+    setSearchText("");
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target) &&
+        menuButtonRef.current &&
+        !menuButtonRef.current.contains(event.target)
+      ) {
+        handleCloseSidebar();
+        handleCloseSearch();
+      }
+    };
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <nav className="sticky top-0 z-50 bg-[#1c1c24] flex md:flex-row flex-col-reverse justify-between gap-6">
+      <p className="text-[#32CD32] align-middle text-center subpixel-antialiased text-3xl font-bold hidden sm:block">
+        College Notes <span className="badge">.tech</span>
+      </p>
+      <Search
+        results={searchedResults}
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onChangeValue={handleSearchChange}
+        setIsPostOpen={setIsPostOpen}
+        setPost={setPost}
+      />
+
+      {/* Small screen navigation */}
+      <div className="sm:hidden flex justify-between items-center relative">
+        <div
+          className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer"
+          onClick={() => router.push("/")}
+        >
+          <Image
+            src={logo}
+            alt="user icon"
+            className="w-[60%] h-[60%] object-contain"
+          />
         </div>
-        <Icon className="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
-        {isOpen && <ShareDialogBox isOpen={isOpen} setIsOpen={setIsOpen} />}
+
+        <p className="text-[#32CD32] align-middle text-center subpixel-antialiased text-3xl font-bold">
+          College Notes<span className="badge">.tech</span>
+        </p>
+
+        <div
+          className={`w-[34px] h-[34px] object-contain cursor-pointer transition-transform transform ${
+            toggleDrawer ? "rotate-45" : "rotate-0"
+          }`}
+          onClick={handleToggleDrawer}
+          ref={menuButtonRef}
+        >
+          <Image
+            src={toggleDrawer ? close : menu}
+            alt="menu icon"
+            className="w-full h-full"
+          />
+        </div>
+
+        <div
+          className={`${
+            toggleDrawer ? "translate-x-0" : "-translate-x-full"
+          } fixed top-0 bottom-0 left-0 rounded-r-[10px] bg-[#1c1c24] z-10 py-5 w-[250px] transition-transform duration-1000`}
+          ref={sidebarRef}
+        >
+          <ul className="mb-4 p-3">
+            {navlinks.map((data) => (
+              <li
+                key={data.name}
+                className={`flex p-4 ${
+                  isActive === data.name && "bg-[#3a3a43]"
+                } hover:bg-[#2c2f32] rounded-full`}
+                onClick={() => {
+                  setIsActive(data.name);
+                  setToggleDrawer(false);
+                  data.btn ? setIsOpen(true) : router.push(data.link);
+                }}
+              >
+                <Image
+                  src={data.imgUrl}
+                  alt={data.name}
+                  className={`w-[24px] h-[24px] object-contain ${
+                    isActive === data.name ? "grayscale-0" : "grayscale"
+                  }`}
+                />
+                <p
+                  className={`ml-[20px] font-epilogue font-semibold text-[14px] ${
+                    isActive === data.name ? "text-[#1dc071]" : "text-[#808191]"
+                  }`}
+                >
+                  {data.name}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </aside>
+      {isOpen && <ShareDialogBox isOpen={isOpen} setIsOpen={setIsOpen} />}
+      {isPostOpen && (
+        <PostViewDialogBox
+          isOpen={isPostOpen}
+          setIsOpen={setIsPostOpen}
+          data={post}
+        />
+      )}
+    </nav>
   );
 };
 
-export default Sidebar;
+export default Navbar;
