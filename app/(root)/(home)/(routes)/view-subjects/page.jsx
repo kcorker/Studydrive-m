e90ef45dbcf5fcf41fedf61bddf3b3d6de81999c
Katherine Corkery <kcorker2@asu.edu>
2024-08-25 14:46:55 +0530

    Update page.jsx fux

--- app/(root)/(home)/(routes)/view-subjects/page.jsx
+++ app/(root)/(home)/(routes)/view-subjects/page.jsx
@@ -49,7 +49,7 @@ const ViewSubjects = () => {
                   <SubCard
                     key={index}
                     hrefData={{
-                      pathname: category === "video" ? "/view-vid" : "/view-doc",
+                      pathname: category === "Videos" ? "/view-vid" : "/view-doc",
                       query: {
                         name: course,
                         category: category,
