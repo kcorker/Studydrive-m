7853ae14e1a7fc47576fe1e9d71243fe3bf19edd
Katherine Corkery <kcorker2@asu.edu>
2024-08-27 00:11:11 +0530

    Update route.js

--- app/api/video/filter/[...filtered]/route.js
+++ app/api/video/filter/[...filtered]/route.js
@@ -7,6 +7,7 @@ export async function GET(req, { params }) {
       where: {
         course_name: courseName,
         semester_code: semester,
+        category: category, 
         subject_code: subId,
       },
     });
